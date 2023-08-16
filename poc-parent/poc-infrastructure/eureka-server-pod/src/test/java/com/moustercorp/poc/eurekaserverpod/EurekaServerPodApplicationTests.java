29a10ecb01b52abdb8342b9209598d2f66632d49
Juan Carlos Bautista <bautista.bjc@gmail.com>
2023-08-16 01:48:15 -0600

    Se implementa el servicio de eureka.

---
+++ poc-parent/poc-infrastructure/eureka-server-pod/src/test/java/com/moustercorp/poc/eurekaserverpod/EurekaServerPodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moustercorp.poc.eurekaserverpod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerPodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
